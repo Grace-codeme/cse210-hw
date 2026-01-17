87b50007ec3a0164585df17f88134a6a965b179a
Grace-codeme <Abdullahiovayozagrace@gmail.com>
2026-01-17 04:43:17 +0100

    Excercise 5 - week 01

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcomeMessage();
+
+        string UserName = PromptForUserName();
+        int UserNum = PromptForUserNum();
+
+        int squaredNum = SquareNum(UserNum);
+        DisplayResult(UserName, squaredNum);
     }
+    static void DisplayWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptForUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptForUserNum()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        return num;
+    }
+    static int SquareNum(int num)
+    {
+        int Square = num * num;
+        return Square;
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+
 }
